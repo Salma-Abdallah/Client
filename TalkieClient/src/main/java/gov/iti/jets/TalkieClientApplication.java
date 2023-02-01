7edb0dbb9af55296550ac493c17268fb843535ba
Soad Ehab <soadehab510@gmail.com>
2023-02-01 17:26:39 +0200

    Adding fxml of notification

--- TalkieClient/src/main/java/gov/iti/jets/TalkieClientApplication.java
+++ TalkieClient/src/main/java/gov/iti/jets/TalkieClientApplication.java
@@ -1,7 +1,22 @@
 package gov.iti.jets;
 
-public class TalkieClientApplication {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.*;
+import javafx.stage.Stage;
 
+public class TalkieClientApplication extends Application {
+    public static void main(String[] args) {
+        Application.launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Home");
+        Parent home = FXMLLoader.load(getClass().getResource("/views/notification.fxml"));
+        Scene scene = new Scene(home);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
     }
 }
